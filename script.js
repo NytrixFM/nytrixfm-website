83ff59c14e1de4b5a1ced30ebad39d8a0f6fad98
NytrixFM <127630632+NytrixFM@users.noreply.github.com>
2025-08-01 10:24:22 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-const userLang = navigator.language || navigator.userLanguage;
-
-if (userLang.toLowerCase().startsWith("fr")) {
-    window.location.href = "fr/index.html";
-} else {
-    window.location.href = "en/index.html";
+const userLang = navigator.language || navigator.userLanguage; // ici la ligne récupère le language préféré de l'utilisateur et le met dans la variavle userLang. En gros si tu est francais belge la variable va être égale a fr-BE.
+if (userLang.toLowerCase().startsWith("fr")) { // ici on vérifie si les deux permières lettres sont fr, car si tu est francais ton language préféré sera fr-XX.
+    window.location.href = "fr/index.html"; // comme tu est francais on ouvre l'index.html francais
+} else { // sinon...
+    window.location.href = "en/index.html"; // l'index englais
 }
+
+// oui je me suis fiat chier a faire des commmentaires donc dis pas que tu as pas compris STP.
+// NytrixFM
