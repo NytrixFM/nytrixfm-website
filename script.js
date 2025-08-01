ef3f59c7cfb0db9e65d46dbc1273388345c035af
NytrixFM <127630632+NytrixFM@users.noreply.github.com>
2025-08-01 10:20:44 +0200

    Create script.js
    Le script js ducoup

---
+++ script.js
@@ -0,0 +1,7 @@
+const userLang = navigator.language || navigator.userLanguage;
+
+if (userLang.toLowerCase().startsWith("fr")) {
+    window.location.href = "fr/index.html";
+} else {
+    window.location.href = "en/index.html";
+}
